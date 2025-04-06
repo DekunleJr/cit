cd5de41cf1678cd232cc19b26e8bd97b4ec955da
DekunleJr <adekunlesa10@gmail.com>
2025-04-06 23:37:24 +0100

    dashboard added

--- route/home.js
+++ route/home.js
@@ -1,60 +1,59 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const isAdmin = require('../util/isAdmin');
-const isAuth = require('../controller/isAuth');
-const controller = require('../controller/home');
+const isAdmin = require("../util/isAdmin");
+const isAuth = require("../controller/isAuth");
+const controller = require("../controller/home");
 
-router.get('/', controller.getindex);
+router.get("/", controller.getindex);
 
-router.get('/about', controller.getAbout);
+router.get("/about", controller.getAbout);
 
-router.get('/contact', controller.getContact);
+router.get("/contact", controller.getContact);
 
-router.get('/courses', controller.getCourses);
+router.get("/courses", controller.getCourses);
 
-router.get('/services', controller.getServices);
+router.get("/services", controller.getServices);
 
-router.get('/events', controller.getEvent);
+router.get("/events", controller.getEvent);
 
-router.get('/news', controller.getNews);
+router.get("/news", controller.getNews);
 
-router.get('/teachers', controller.getTeacher);
+router.get("/teachers", controller.getTeacher);
 
-router.get('/myCourses', isAuth, controller.getMyCourses);
+router.get("/myCourses", isAuth, controller.getMyCourses);
 
-router.get('/admin', isAdmin, controller.getAdmin);
+router.get("/admin", isAdmin, controller.getAdmin);
 
+router.post("/admin/teacher", controller.postTeachers);
 
-router.post('/admin/teacher', controller.postTeachers);
+router.post("/admin/event", controller.postEvent);
 
-router.post('/admin/event', controller.postEvent);
+router.post("/admin/news", controller.postNews);
 
-router.post('/admin/news', controller.postNews);
+router.post("/admin/course", controller.postCourse);
 
-router.post('/admin/course', controller.postCourse);
+router.post("/admin/service", controller.postService);
 
-router.post('/admin/service', controller.postService);
+router.post("/admin/alumni", controller.postAlumni);
 
-router.post('/admin/alumni', controller.postAlumni);
+router.post("/send_mail", controller.postEmail);
 
-router.post('/send_mail', controller.postEmail);
+router.post("/delete-news", isAdmin, controller.postDeleteNews);
 
-router.post('/delete-news', isAdmin, controller.postDeleteNews);
+router.post("/delete-event", isAdmin, controller.postDeleteEvent);
 
-router.post('/delete-event', isAdmin, controller.postDeleteEvent);
+router.post("/delete-teacher", isAdmin, controller.postDeleteTeacher);
 
-router.post('/delete-teacher', isAdmin, controller.postDeleteTeacher);
+router.post("/delete-course", isAdmin, controller.postDeleteCourse);
 
-router.post('/delete-course', isAdmin, controller.postDeleteCourse);
+router.post("/delete-service", isAdmin, controller.postDeleteService);
 
-router.post('/delete-service', isAdmin, controller.postDeleteService);
+router.post("/delete-alumni", isAdmin, controller.postDeleteAlumni);
 
-router.post('/delete-alumni', isAdmin, controller.postDeleteAlumni);
+router.post("/initialize-payment", controller.postPayment);
 
-router.get('/:courseId', isAuth, controller.getCourse);
+router.get("/payment/callback", controller.getPayment);
 
-router.post('/initialize-payment', controller.postPayment);
+router.get("/:courseId", isAuth, controller.getCourse);
 
-router.get('/payment/callback', controller.getPayment)
-
-module.exports = router
+module.exports = router;
